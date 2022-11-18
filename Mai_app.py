78490266c4634ed8bae650016f58bc498e7ca650
Shafaq Mughal <60179208+ShafaqFatimaMughal@users.noreply.github.com>
2022-11-18 12:42:55 +0500

    Update Mai_app.py

--- Mai_app.py
+++ Mai_app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 # model = pickle.load(open('mai_model.pkl', 'rb'))
 # tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
 
-@app.route('/reply', methods=['GET'])
+# @app.route('/reply', methods=['GET'])
+@app.route('/', methods=['GET'])
 def reply():
     
 #     data = request.get_json(force=True) 
